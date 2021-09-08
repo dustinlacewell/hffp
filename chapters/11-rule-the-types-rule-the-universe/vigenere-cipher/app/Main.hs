264a74bc90e4c4d8aaf21986b037faaff2cef93e
Dustin Lacewell <dlacewell@gmail.com>
2021-09-07 21:24:53 -0500

    Make vigenere cipher behave exactly like book

--- chapters/11-rule-the-types-rule-the-universe/vigenere-cipher/app/Main.hs
+++ chapters/11-rule-the-types-rule-the-universe/vigenere-cipher/app/Main.hs
@@ -1,33 +1,106 @@
+module VigenereCipher where
+
 import Data.Char
-import Data.Sequence ( mapWithIndex )
+import Data.Function
 
 type Key = String
 
-rotate :: Int -> Char -> Char
-rotate n c = chr $ ord c + n
+-- | Feed the output of a function back into itself n times
+-- This is a test helper function
+-- >>> churn (+1) 0 5
+-- 5
+churn f a 0 = a
+churn f a n = churn f (f a) (n - 1)
 
-nForChar :: Char -> Int
-nForChar c = ord c - ord 'a'
+baseOrdinal = ord 'a'
 
-mapWithIdx f = zipWith f [0..]
+-- | Compute ordinal relative to 'a'
+-- >>> debaseOrdinal 'a'
+-- 0
+-- >>> debaseOrdinal 'z'
+-- 25
+debaseOrdinal c = ord c - baseOrdinal
 
-crypt :: (Char -> Int) -> Key -> String -> String
-crypt nf k =
-    mapWithIdx (\i c ->
-        let j = k !! (i `mod` length k)
-            n = nf j
-        in rotate n c)
+-- | Rotate a character by a given number of places
+-- >>> rotate 0 'a'
+-- 'a'
+-- >>> rotate 1 'a'
+-- 'b'
+-- >>> rotate 1 'z'
+-- 'a'
+rotate amount char = chr $
+    debaseOrdinal char
+    & (+ amount)
+    & (`mod` 26)
+    & (+ baseOrdinal)
 
-encrypt :: Key -> String -> String
-encrypt = crypt nForChar
+-- | cycle key by 1, return tip and cycled key
+-- >>> cycleKey "ally"
+-- ('a',"llya")
+-- >>> churn (\(c, k) -> cycleKey k) ('y', "ally") 4
+-- ('y',"ally")
+cycleKey key = (keyChar, newKey) where
+    (keyChar:restKey) = key
+    newKey = restKey ++ [keyChar]
 
-decrypt :: Key -> String -> String
-decrypt = crypt (negate . nForChar)
+-- | Crypt a valid character using the given key and offset function
+-- >>> handleChar debaseOrdinal "az" "a" ""
+-- State {key = "za", input = "", output = "a"}
+handleChar offsetFun key input output =
+    State key' input' output'
+    where
+        char : input' = input
+        (keyChar, key') = cycleKey key
+        offset = offsetFun keyChar
+        char' = rotate offset char
+        output' = output ++ [char']
 
-main :: IO ()
+-- | Skip the current character since it's not a valid character
+-- >>> skipChar "ab" " b" "a"
+-- State {key = "ab", input = "b", output = "a "}
+skipChar key input output =
+    State key rest $ output ++ [char]
+    where char:rest = input
+
+data State = State {
+    key :: Key,
+    input :: String,
+    output :: String
+} deriving (Show)
+
+-- | Encrypt a single character in the State
+-- >>> worker debaseOrdinal (State "a" "b" "")
+-- State {key = "a", input = "", output = "b"}
+-- >>> worker debaseOrdinal (State "a" " b" "a")
+-- State {key = "a", input = "b", output = "a "}
+worker offsetFun (State key input output) =
+    if isAlpha $ head input then
+        handleChar offsetFun key input output
+    else
+        skipChar key input output
+
+-- | Process input until it's empty
+work f (State key input output)
+    | input == "" = State key input output
+    | otherwise = work f $ f (State key input output)
+
+-- | Crypt a string using the given key
+crypt processor key input =
+    work processor (State key input "") & output
+
+-- | Encrypt a string using the given key
+-- >>> encrypt "ally" "meet at dawn"
+-- "mppr ae oywy"
+encrypt = crypt (worker debaseOrdinal)
+
+-- | Decrypt a string using the given key
+-- >>> decrypt "ally" "mppr ae oywy"
+-- "meet at dawn"
+decrypt = crypt $ worker (negate . debaseOrdinal)
+
 main =
-    let key = "foxhound"
-        plaintext = "All your base are belong to us"
+    let key = "ally"
+        plaintext = "meet at dawn"
         ciphertext = encrypt key plaintext
         decrypted = decrypt key ciphertext
     in do
@@ -35,4 +108,3 @@ main =
         putStrLn $ "Plaintext: " ++ plaintext
         putStrLn $ "Ciphertext: " ++ ciphertext
         putStrLn $ "Decrypted: " ++ decrypted
-
